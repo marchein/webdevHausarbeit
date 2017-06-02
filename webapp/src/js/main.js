74c4e7fdcf0abf249277ffd0465192a70eedcba9
Marc Hein <info@marc-hein.de>
2017-06-02 12:03:14 +0200

    Reorganized main.js & optimized build process

--- webapp/src/js/main.js
+++ webapp/src/js/main.js
@@ -41,8 +41,6 @@ map.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token
 	accessToken: "pk.eyJ1IjoibWFyY2hlaW4iLCJhIjoiY2ozNHF6bm9tMDAyajJ3cDdjYWQ5N3QydCJ9.I5KDSfeFcn6e2oUJi6k2fg"
 }).addTo(mapView);
 
-functions.loadFile(serverPath + "api/tracks", init); // load all tracks and call init()
-
 function init() {
 	allTracks = JSON.parse(this.responseText); // parse response
 	/*for (let i = 0; i < allTracks.length; i++) {
@@ -57,13 +55,108 @@ function init() {
 	right.addEventListener("click", nextPage, true);
 }
 
-function addTracksToList(name, id) {
-	let trackButton = document.createElement("span"); // create track element
-	trackButton.innerHTML = name; // set name
-	trackButton.className = "trackButton"; // set class
-	trackButton.id = id; // set id to it's own id (this is == the id in the api)
-	tracks.appendChild(trackButton); // add to the list
-	trackButton.addEventListener("click", loadSelectedTrack, false); // on click load infomation of the track
+functions.loadFile(serverPath + "api/tracks", init); // load all tracks and call init()
+
+let mapLayer;
+
+function addMarkers(coordinates) {
+	if (mapLayer !== undefined) {
+		let greenIcon = new map.Icon({
+			iconUrl: "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
+			shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+			iconSize: [25, 41],
+			iconAnchor: [12, 41],
+			popupAnchor: [1, -34],
+			shadowSize: [41, 41]
+		});
+
+		let redIcon = new map.Icon({
+			iconUrl: "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
+			shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+			iconSize: [25, 41],
+			iconAnchor: [12, 41],
+			popupAnchor: [1, -34],
+			shadowSize: [41, 41]
+		});
+
+		map.marker([coordinates[0][1], coordinates[0][0]], { icon: redIcon }).addTo(mapLayer);
+		map.marker([coordinates[coordinates.length - 1][1], coordinates[coordinates.length - 1][0]], { icon: greenIcon }).addTo(mapLayer);
+	}
+}
+function addCanvas() {
+	if (profileCanvas !== undefined) {
+		profileContainer.removeChild(profileCanvas);
+	}
+	if (profileContainer !== undefined) {
+		mapArea.removeChild(profileContainer);
+	}
+
+	profileContainer = document.createElement("div");
+	profileContainer.id = "profileContainer";
+	mapArea.appendChild(profileContainer);
+
+	profileCanvas = document.createElement("canvas"); //create map area
+	profileCanvas.id = "profileCanvas";
+	profileCanvas.width = 700;
+	profileCanvas.height = 300;
+	profileContainer.appendChild(profileCanvas);
+}
+
+function drawHeight(coordinates) {
+	let heightPoints = [];
+	var ctx = profileCanvas.getContext("2d");
+	ctx.transform(1, 0, 0, -1, 0, profileCanvas.height);
+
+	for (let i = 0; i < coordinates.length; i++) {
+		heightPoints.push(coordinates[i][2]);
+	}
+
+	ctx.beginPath();
+	ctx.lineWidth = 1;
+	ctx.fillStyle = "white";
+	ctx.strokeStyle = "white";
+
+	let min = Math.min.apply(Math, heightPoints);
+	let max = Math.max.apply(Math, heightPoints);
+	let diff = max - min;
+
+	let height = document.getElementById("profileCanvas").height;
+	let width = document.getElementById("profileCanvas").width;
+
+	ctx.moveTo(0, 0);
+
+	for (let i = 0; i < heightPoints.length; i++) {
+		ctx.lineTo(((i / heightPoints.length) * width), ((heightPoints[i] - min) / diff) * height);
+	}
+
+	ctx.lineTo(width, 0);
+	ctx.closePath();
+	ctx.fill();
+}
+
+function highlightSelectedTrack(id) {
+	if (mapLayer !== undefined) {
+		mapView.removeLayer(mapLayer);
+	}
+	addCanvas();
+	//let selectedTrack = JSON.parse(this.responseText); // get answer from api
+	let selectedTrack = allTracks[id];
+	// console.log(selectedTrack.features[0].geometry.coordinates); // output the name of the response
+	var style = {
+		color: "#ff0000",
+		weight: 5
+	};
+
+	mapLayer = map.geoJSON(selectedTrack, {
+		style: style
+	});
+
+	let coordinates = selectedTrack.features[0].geometry.coordinates;
+	addMarkers(coordinates);
+	drawHeight(coordinates);
+
+	mapView.fitBounds(mapLayer.getBounds());
+	mapLayer.addTo(mapView);
 }
 
 function loadSelectedTrack(event) {
@@ -77,6 +170,15 @@ function loadSelectedTrack(event) {
 	let id = event.target.id; // get id from <span id="THIS IS THIS ID">
 	highlightSelectedTrack(id);
 	//functions.loadFile(serverPath + "api/tracks/" + id, highlightSelectedTrack); // load from the api
+}
+
+function addTracksToList(name, id) {
+	let trackButton = document.createElement("span"); // create track element
+	trackButton.innerHTML = name; // set name
+	trackButton.className = "trackButton"; // set class
+	trackButton.id = id; // set id to it's own id (this is == the id in the api)
+	tracks.appendChild(trackButton); // add to the list
+	trackButton.addEventListener("click", loadSelectedTrack, false); // on click load infomation of the track
 }
 
 function backPage() {
@@ -152,108 +254,4 @@ function setControls() {
 	}
 
 	lastItems = nextItems;
-}
-
-let mapLayer;
-
-function highlightSelectedTrack(id) {
-	if (mapLayer !== undefined) {
-		mapView.removeLayer(mapLayer);
-	}
-	addCanvas();
-	//let selectedTrack = JSON.parse(this.responseText); // get answer from api
-	let selectedTrack = allTracks[id];
-	// console.log(selectedTrack.features[0].geometry.coordinates); // output the name of the response
-	var style = {
-		color: "#ff0000",
-		weight: 5
-	};
-
-	mapLayer = map.geoJSON(selectedTrack, {
-		style: style
-	});
-
-	let coordinates = selectedTrack.features[0].geometry.coordinates;
-	addMarkers(coordinates);
-	drawHeight(coordinates);
-
-	mapView.fitBounds(mapLayer.getBounds());
-	mapLayer.addTo(mapView);
-}
-
-function addMarkers(coordinates) {
-	if (mapLayer !== undefined) {
-		let greenIcon = new map.Icon({
-			iconUrl: "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
-			shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
-			iconSize: [25, 41],
-			iconAnchor: [12, 41],
-			popupAnchor: [1, -34],
-			shadowSize: [41, 41]
-		});
-
-		let redIcon = new map.Icon({
-			iconUrl: "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
-			shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
-			iconSize: [25, 41],
-			iconAnchor: [12, 41],
-			popupAnchor: [1, -34],
-			shadowSize: [41, 41]
-		});
-
-		map.marker([coordinates[0][1], coordinates[0][0]], { icon: redIcon }).addTo(mapLayer);
-		map.marker([coordinates[coordinates.length - 1][1], coordinates[coordinates.length - 1][0]], { icon: greenIcon }).addTo(mapLayer);
-	}
-}
-
-function drawHeight(coordinates) {
-	let heightPoints = [];
-	var ctx = profileCanvas.getContext("2d");
-	ctx.transform(1, 0, 0, -1, 0, profileCanvas.height);
-
-	for (let i = 0; i < coordinates.length; i++) {
-		heightPoints.push(coordinates[i][2]);
-	}
-
-	ctx.beginPath();
-	ctx.lineWidth = 1;
-	ctx.fillStyle = "white";
-	ctx.strokeStyle = "white";
-
-	let min = Math.min.apply(Math, heightPoints);
-	let max = Math.max.apply(Math, heightPoints);
-	let diff = max - min;
-
-	let height = document.getElementById("profileCanvas").height;
-	let width = document.getElementById("profileCanvas").width;
-	console.log(width);
-
-	ctx.moveTo(0, 0);
-
-	for (let i = 0; i < heightPoints.length; i++) {
-		ctx.lineTo(((i / heightPoints.length) * width), ((heightPoints[i] - min) / diff) * height);
-	}
-
-	ctx.lineTo(width, 0);
-	ctx.closePath();
-	ctx.fill();
-}
-
-function addCanvas() {
-	if (profileCanvas !== undefined) {
-		profileContainer.removeChild(profileCanvas);
-	}
-	if (profileContainer !== undefined) {
-		mapArea.removeChild(profileContainer);
-	}
-
-	profileContainer = document.createElement("div");
-	profileContainer.id = "profileContainer";
-	mapArea.appendChild(profileContainer);
-
-	profileCanvas = document.createElement("canvas"); //create map area
-	profileCanvas.id = "profileCanvas";
-	profileCanvas.width = 700;
-	profileCanvas.height = 300;
-	profileContainer.appendChild(profileCanvas);
 }
